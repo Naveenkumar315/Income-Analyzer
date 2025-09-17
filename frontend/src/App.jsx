9762145e48ab4a64fbcb92dc75c99278457818a8
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2025-09-17 14:42:54 +0530

    frontend creation 2.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      <div className=''>
-       
-      </div>
-    </>
-  )
-}
-
-export default App
+const App = () => {
+  return <div className="text-3xl font-bold underline">Hello, world!</div>;
+};
+export default App;
