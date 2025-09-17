102c482531c0278d84f7625ebc501204936aef45
Naveen <naveenkumarm572@gmail.com>
2025-09-17 17:14:03 +0530

    version 1 added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from './components/Button'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
       <div className=''>
-        {/* <Button variant="primary" label={'Click'}/> */}
-        <div className='m-10'>
-
-        <Button variant="cancel" label='Continue'/>
-        </div>
+        <Home/>
       </div>
     </>
   )
