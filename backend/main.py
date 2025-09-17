54f6c1a26d29c6b76a8f35a3e63422f7f8ec7185
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2025-09-17 16:48:30 +0530

    part 2 backend auth

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth
 
 app = FastAPI(title="Income Analyzer API", version="1.0.0")
 
 app.include_router(auth.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # which origins are allowed
+    allow_credentials=True,
+    allow_methods=["*"],              # allow all HTTP methods
+    allow_headers=["*"],              # allow all headers
+)
 
 @app.get("/")
 async def root():
